aa1616bf5cbda799e6a2780f86ebf390eefbc48b
Letter May Mulles <maymulles123@gmail.com>
2020-09-18 03:12:58 +0800

    first commit

---
+++ resources/views/pages/information.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lettermay_Mulles</title>
+</head>
+<body>
+
+<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}" >
+
+<div class="card" style="width: 50rem; margin-top: 100px; padding: 30px; margin-left: 300px;">
+  <div class="card-body">
+  </div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <a class="navbar-brand" href="home">HOME</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="student">STUDENT <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="information">INFORMATION</a>
+    
+
+      
+    </div>
+  </div>
+</nav>
+</p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+  </div>
+
+
+</body>
+
+
+
+
+
+
+
+</html>
