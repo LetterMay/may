3df77ecb68648b2e275f6573e409af8e557f0d44
Lettermay <maymulles123@gmail.com>
2020-10-06 16:58:02 +0800

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function trainor(){
+        return $this->hasMany('App\Trainor');
+    }
+    public function trainee(){
+        return $this->hasMany('App\Trainee');
+    }
 }
